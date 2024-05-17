380eb47bc9e778d99e99e7644bd7354f752445d2
Theoness1 <sasha.bolotskih2002@gmail.com>
2024-05-17 15:59:39 +0700

    fix for 1.20.6

--- src/main/java/ru/tpsd/eatinganimationmod/mixin/DrawContextMixin.java
+++ src/main/java/ru/tpsd/eatinganimationmod/mixin/DrawContextMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -11,6 +12,7 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
@@ -22,21 +24,22 @@ public abstract class DrawContextMixin {
 	@SuppressWarnings(value = "all")
 	@ModifyVariable(method = "drawItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;IIII)V", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/render/item/ItemRenderer;getModel(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;I)Lnet/minecraft/client/render/model/BakedModel;"))
 	private BakedModel getModel(BakedModel bakedModel, @Nullable LivingEntity entity, @Nullable World world, ItemStack stack, int x, int y, int seed, int z) {
-		if(stack.isFood()){
-			return this.getHeldItemModel(stack, null, entity, seed);
+		if(stack.contains(DataComponentTypes.FOOD)){
+			return this.getHeldFoodItemModel(stack, null, entity, seed);
 		}
 		return this.client.getItemRenderer().getModel(stack, world, entity, seed);
 	}
 
-	public BakedModel getHeldItemModel(ItemStack stack, @Nullable World world, @Nullable LivingEntity entity, int seed) {
+	@Unique
+	public BakedModel getHeldFoodItemModel(ItemStack stack, @Nullable World world, @Nullable LivingEntity entity, int seed) {
 
-		BakedModel bakedModel = client.getItemRenderer().getModels().getModel(stack);
+		BakedModel currentItemBakedModel = client.getItemRenderer().getModels().getModel(stack);
 
 		ClientWorld clientWorld = world instanceof ClientWorld ? (ClientWorld)world : null;
-		BakedModel bakedModel1 = stack.isFood()
+		BakedModel itemBakedModel = stack.contains(DataComponentTypes.FOOD)
 				? client.getItemRenderer().getModels().getModel(stack)
-				: bakedModel.getOverrides().apply(bakedModel, stack, clientWorld, entity, seed);
+				: currentItemBakedModel.getOverrides().apply(currentItemBakedModel, stack, clientWorld, entity, seed);
 
-		return bakedModel1 == null ? client.getItemRenderer().getModels().getModelManager().getMissingModel() : bakedModel1;
+		return itemBakedModel == null ? client.getItemRenderer().getModels().getModelManager().getMissingModel() : itemBakedModel;
 	}
 }
