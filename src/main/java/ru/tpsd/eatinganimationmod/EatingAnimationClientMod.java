087162d533c4e60ca069a812b12ddbd1f0c64147
Theoness1 <sasha.bolotskih2002@gmail.com>
2024-05-17 16:01:54 +0700

    Fixed eating desync with the players

--- src/main/java/ru/tpsd/eatinganimationmod/EatingAnimationClientMod.java
+++ src/main/java/ru/tpsd/eatinganimationmod/EatingAnimationClientMod.java
@@ -1,11 +1,12 @@
 package ru.tpsd.eatinganimationmod;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.client.network.OtherClientPlayerEntity;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
@@ -15,38 +16,38 @@ import java.util.ArrayList;
 
 public class EatingAnimationClientMod implements ClientModInitializer {
 
-   public static float itemUseTime = 0;
-   private static final ArrayList<Item> foodItems = new ArrayList<>(Registries.ITEM.stream().filter(Item::isFood).toList());
+    private static final ArrayList<Item> FOOD_ITEMS = new ArrayList<>(Registries.ITEM.stream().filter(
+            p -> p.getDefaultStack().getComponents().contains(DataComponentTypes.FOOD)).toList());
    static {
-       foodItems.add(Items.MILK_BUCKET);
+       FOOD_ITEMS.add(Items.MILK_BUCKET);
    }
-
-   // asbyth thank you bru! ඞ
 
     @Override
     public void onInitializeClient() {
-        for (Item item : foodItems) {
-            FabricModelPredicateProviderRegistry.register(item, new Identifier("eat"), (itemStack, clientWorld, livingEntity, i) -> {
+        for (Item item : FOOD_ITEMS) {
+            ModelPredicateProviderRegistry.register(item, new Identifier("eat"), (itemStack, clientWorld, livingEntity, i) -> {
                 if (livingEntity == null) {
                     return 0.0F;
                 }
-
-                if(livingEntity instanceof OtherClientPlayerEntity && livingEntity.getItemUseTime() > 31){
-                    return itemUseTime / 30;
+                if(livingEntity instanceof OtherClientPlayerEntity) {
+                    if(itemStack.getMaxUseTime() > 16) {
+                        return livingEntity.getActiveItem() != itemStack ? 0.0F : ((float)livingEntity.getItemUseTime() / (float)itemStack.getMaxUseTime()) % 1;
+                    }
+                    else {
+                        return livingEntity.getActiveItem() != itemStack ? 0.0F : ((float)livingEntity.getItemUseTime() / 32.0f) % 0.5F;
+                    }
                 }
                 return livingEntity.getActiveItem() != itemStack ? 0.0F : (itemStack.getMaxUseTime() - livingEntity.getItemUseTimeLeft()) / 30.0F;
             });
 
-            FabricModelPredicateProviderRegistry.register(item, new Identifier("eating"), (itemStack, clientWorld, livingEntity, i) -> {
+            ModelPredicateProviderRegistry.register(item, new Identifier("eating"), (itemStack, clientWorld, livingEntity, i) -> {
                 if (livingEntity == null) {
                     return 0.0F;
                 }
-
                 return livingEntity.isUsingItem() && livingEntity.getActiveItem() == itemStack ? 1.0F : 0.0F;
             });
 
         }
-
         FabricLoader.getInstance().getModContainer("eatinganimationid").ifPresent(eatinganimation ->
                 ResourceManagerHelper.registerBuiltinResourcePack(EatingAnimationClientMod.locate("supporteatinganimation"), eatinganimation, ResourcePackActivationType.DEFAULT_ENABLED));
     }
